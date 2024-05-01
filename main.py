128f2734c24da8d981a2051a94b9d6da6b052bec
Esha Gusain <eshgsn@gmail.com>
2024-05-01 23:20:44 +0530

    minor updates

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from pymongo.errors import DuplicateKeyError
 from bson import ObjectId
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Query
+
 
 app = FastAPI()
 
@@ -23,13 +25,15 @@ client = AsyncIOMotorClient("mongodb://localhost:27017")
 db = client["project1"]
 collection = db["argenie"]
 
+
+
 class DemoModel(BaseModel):
-    user_id: str
-    user_name: str
-    first_name: str
-    last_name: str
-    email: str
-    phone_number: str
+    movie :str
+    title :str
+    genres :str
+    year :str
+    Rating :str
+    RottenTomato :str
 
 
 
@@ -44,23 +48,22 @@ async def create_demo(demo: DemoModel):
         raise HTTPException(status_code=400, detail=str(e))
 
 
-
 @app.get("/demo/")
-async def read_demo():
+async def read_demo(page: int = Query(1, gt=0), per_page: int = Query(10, gt=0)):
+    skip_count = (page - 1) * per_page
     users = []
-    async for user in collection.find({}):
+    async for user in collection.find({}).skip(skip_count).limit(per_page):
         user['_id'] = str(user['_id'])  # Convert ObjectId to string
         users.append(user)
     return users
-    # raise HTTPException(status_code=404, detail="Demo not found")
 
 
 
 @app.put("/demo/{demo_id}")
 async def update_demo(demo_id: str, demo: DemoModel):
     demo_dict = demo.dict()
-    await collection.update_one({"user_id": demo_id}, {"$set": demo_dict})
-    updated_demo = await collection.find_one({"user_id": demo_id})
+    await collection.update_one({"_id": demo_id}, {"$set": demo_dict})
+    updated_demo = await collection.find_one({"_id": demo_id})
     if updated_demo:
         return updated_demo
     raise HTTPException(status_code=404, detail="Demo not found")
@@ -69,10 +72,9 @@ async def update_demo(demo_id: str, demo: DemoModel):
 
 @app.delete("/demo/{demo_id}")
 async def delete_demo(demo_id: str):
-    deleted_demo = await collection.find_one_and_delete({"user_id": demo_id})
+    deleted_demo = await collection.find_one_and_delete({"_id": demo_id})
     if deleted_demo:
         return deleted_demo
     raise HTTPException(status_code=404, detail="Demo not found")
-
 
 
